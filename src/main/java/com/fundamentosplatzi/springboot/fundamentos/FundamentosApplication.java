ebd122190b04b1de33c8157c05732e15f4df2f56
earamosb8 <earamosb8@gmail.com>
2022-05-16 23:39:01 -0500

    second commit

--- src/main/java/com/fundamentosplatzi/springboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos/FundamentosApplication.java
@@ -1,13 +1,33 @@
 package com.fundamentosplatzi.springboot.fundamentos;
-
+import com.fundamentosplatzi.springboot.fundamentos.bean.MyBean;
+import com.fundamentosplatzi.springboot.fundamentos.bean.MyBeanWithDependency;
+import com.fundamentosplatzi.springboot.fundamentos.component.ComponentDependency;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FundamentosApplication {
+public class FundamentosApplication implements CommandLineRunner {
+//inyectamos nuestra dependencia
+	private ComponentDependency componentDependency;
+	private MyBean myBean;
+	private MyBeanWithDependency myBeanWithDependency;
 
-	public static void main(String[] args) {
-		SpringApplication.run(FundamentosApplication.class, args);
+	public FundamentosApplication(@Qualifier("componentTwoImplement") ComponentDependency componentDependency, MyBean myBean,MyBeanWithDependency myBeanWithDependency){
+		this.componentDependency = componentDependency;
+		this.myBean = myBean;
+		this.myBeanWithDependency = myBeanWithDependency;
 	}
 
+	public static void main(String[] args) {
+
+		SpringApplication.run(FundamentosApplication.class, args);
+	}
+//metodo run ejecuta
+	public void run(String... args) throws Exception {
+		componentDependency.saludar();
+		myBean.print();
+		myBeanWithDependency.printWithDependency();
+	}
 }
---
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos/bean/MyBeanWithDependencyImplement.java
@@ -0,0 +1,15 @@
+package com.fundamentosplatzi.springboot.fundamentos.bean;
+
+public class MyBeanWithDependencyImplement implements MyBeanWithDependency{
+
+    private MyOperation myOperation;
+    public MyBeanWithDependencyImplement( MyOperation myOperation){
+        this.myOperation = myOperation;
+    }
+    @Override
+    public void printWithDependency() {
+        int numero=1;
+        System.out.println(myOperation.sum(numero));
+
+    }
+}
